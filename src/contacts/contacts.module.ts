c758e39cf0f7a45d57871cbd887b05c5e27df57d
Darshan Chobarkar <dchobarkar@gmail.com>
2024-02-08 17:35:44 +0530

    add typeorm and sqlite3

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ContactsService } from './contacts/contacts.service';
 import { ContactsController } from './contacts/contacts.controller';
-
+import { Contact } from './contact.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Contact])],
   providers: [ContactsService],
   controllers: [ContactsController],
 })
