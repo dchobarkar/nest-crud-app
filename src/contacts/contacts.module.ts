edf22e022f3b8ede323e3e44d3ef13a89c47dd71
Darshan Chobarkar <dchobarkar@gmail.com>
2024-02-08 17:48:36 +0530

    complete contacts module

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { ContactsService } from './contacts/contacts.service';
-import { ContactsController } from './contacts/contacts.controller';
+import { ContactsService } from './contacts.service';
+import { ContactsController } from './contacts.controller';
 import { Contact } from './contact.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([Contact])],
