a402082b5efbbc9de4136c697ee27458f6a1a0cd
Darshan Chobarkar <dchobarkar@gmail.com>
2024-02-08 17:59:58 +0530

    add information

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
